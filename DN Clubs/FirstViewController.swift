d41a08e3475f39a3119d74f01bd72b0af9254920
DN Student <dnstudent@00-21132.local>
2015-04-23 16:09:12 -0700

    Initial Commit

---
+++ DN Clubs/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  DN Clubs
+//
+//  Created by DN Student on 4/23/15.
+//  Copyright (c) 2015 Nighthackers. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
