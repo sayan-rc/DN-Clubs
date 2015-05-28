322ec4783aa0b053f4b27b793384330a7f9d3fd9
DN Student <dnstudent@00-21156.local>
2015-05-28 16:20:17 -0700

    Attempt "Hai there :3"

---
+++ DN Clubs/TableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  TableViewCell.swift
+//  DN Clubs
+//
+//  Created by DN Student on 5/28/15.
+//  Copyright (c) 2015 Nighthackers. All rights reserved.
+//
+
+import Cocoa
+
+class TableViewCell: UITableViewCell {
+
+}
